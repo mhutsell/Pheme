7fe74a121585fb4f7994942f3b61797af3118118
Chen Gong <gongcchen999@gmail.com>
2021-11-10 21:35:53 -0500

    added login page

---
+++ StorageV1/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  StorageV1
+//
+//  Created by 龚晨 on 11/10/21.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
